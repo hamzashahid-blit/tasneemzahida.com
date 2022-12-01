f9e2da2190ce3dfccb99dcd6eff4855e26b0a8f8
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2022-12-01 15:33:33 +0500

    Responsive homepage

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,20 +3,24 @@
 </script>
 
 <!-- HIEGHT OF NAVBAR: theme(space.28) -->
-<nav class="flex justify-evenly items-center text-primary h-28">
-  <div class="bg-softgray w-[300px] h-[310px] rounded-full scale-x-125 absolute -z-10 top-[-180px]"></div>
-  <ul class="flex justify-evenly w-full items-center">
+<nav class="flex justify-evenly items-center text-primary h-28" class:active>
+  <div class="bg-softgray w-[300px] h-[310px] lg:w-[300px] lg:h-[310px] rounded-full scale-x-125 absolute -z-10 top-[calc(-22vh+3vw)] sm:top-[calc(-22vh)]"></div>
+  <!-- <div class="bg-softgray w-[70vw] h-[55vw] rounded-full absolute -z-10 -top-[calc(55vw)/2]"></div> -->
+  <ul class="order-3 lg:order-1">
     <li data-sveltekit-prefetch><a href="/visual">Visual Da'wah</a></li>
     <li data-sveltekit-prefetch><a href="/calligraphy">Calligraphy</a></li>
     <li data-sveltekit-prefetch><a href="/canonical">Canonical</a></li>
   </ul>
-  <a href="/" class="w-[45rem]"><img src="/logo-small.png" alt="Logo"></a>
-  <ul class="flex justify-evenly w-full items-center">
+  <!--  translate-x-[calc(50vw-50%)] lg:translate-x-0 -->
+  <a href="/" class="lg:order-1 max-w-[18em] w-[60vw] mx-5
+					 translate-x-[calc(50vw-50%-theme(space.5))] lg:translate-x-0">
+	<img src="/logo-small.png" alt="Logo">
+  </a>
+  <ul class="order-4 lg:order-3">
 	<li data-sveltekit-prefetch><a href="/events">Events</a></li>
 	<li data-sveltekit-prefetch><a href="/contacts">Contact Me</a></li>
   </ul>
-  <button class="hamburger mr-4 flex flex-col space-y-1"
-		  class:active
+  <button class="lg:absolute lg:invisible order-2 lg:order-last hamburger ml-auto mr-4 flex flex-col space-y-1"
 		  on:click="{() => active = !active}">
     <div class="bar"></div>
     <div class="bar"></div>
@@ -28,6 +32,9 @@
   li {
     font-size: 1.2em;
   }
+  ul {
+	@apply flex flex-col lg:flex-row justify-evenly w-full items-center absolute lg:relative -top-[100%] lg:top-0
+  }
   .bar {
 	@apply w-7 h-[.3rem] bg-primary rounded-xl transition-all ease-in-out
   }
@@ -35,5 +42,8 @@
 	/* Spacing = 0.25rem, Thickness = 0.3rem, Correction = 0.5px */
 	@apply even:opacity-0 first:-rotate-45 last:rotate-45
 		   first:translate-y-[calc(0.3rem+0.25rem-.5px)] last:translate-y-[calc(-0.3rem-0.25rem)]
+  }
+  .active ul {
+	@apply bg-black
   }
 </style>
--- src/routes/AboutMe.svelte
+++ src/routes/AboutMe.svelte
@@ -1,8 +1,8 @@
-<div id="about-me" class="flex flex-col-reverse lg:flex-row items-center justify-evenly md:space-x-20 text-primary h-[calc(100vh-theme(space.28))] -mt-10 mb-36 mx-6 md:mx-20">
-  <div class="flex flex-col">
-    <h1 class="text-5xl md:text-6xl mb-2 -mt-4">About <span class="font-fancy text-5xl md:text-7xl -ml-2">me</span></h1>
+<div id="about-me" class="flex flex-col lg:flex-row items-center lg:justify-evenly lg:space-x-20 text-primary min-h-[calc(100vh-theme(space.28))] lg:-mt-10 mb-36 mx-6 lg:mx-20">
+  <div class="order-2 lg:order-1 flex flex-col">
+    <h1 class="text-5xl lg:text-6xl mb-2 -mt-4">About <span class="font-fancy text-5xl lg:text-7xl -ml-2">me</span></h1>
 	<p class="text-2xl max-w-2xl">Cool stuff and description omg very cool. We all love some stuff yeah. Nice thing mate. I think about all things sometimes once in a while.  Love art in general. Thanks for reading.</p>
   </div>
-  <img class="h-57 rounded-xl drop-shadow-lg max-w-full block md:my-0 my-16" src="/about-me.jpg" alt="Portrait">
+  <img class="order-1 lg:order-2 max-h-57 rounded-xl drop-shadow-lg max-w-full flex lg:my-0 my-16" src="/about-me.jpg" alt="Portrait">
 <!-- 5px_5px_0px_20px_rgb(100,20,200,1) -->
 </div>
