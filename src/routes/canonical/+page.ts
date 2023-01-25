3de3098a5eba0338e747b03f5285d59e3ac66496
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2023-01-25 12:18:45 +0500

    Completely remove paintings API

--- src/routes/canonical/+page.ts
+++ src/routes/canonical/+page.ts
@@ -1,7 +1,17 @@
+import { error, json } from '@sveltejs/kit';
+import allPaintings from '$lib/assets/paintings.json';
 import type { PageLoad } from './$types';
 
+export const prerender = true;
+export const ssr = true;
+export const csr = true;
+
 export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch('/api/paintings/canonical');
+	let canonicalPaintings = allPaintings.filter((painting) => painting.category === 'canonical');
+	if (!canonicalPaintings) {
+		throw error(404, 'No canonical paintings found...');
+	}
+	const response = new json(canonicalPaintings);
 	const paintings = await response.json();
 	return {
 		paintings: paintings
