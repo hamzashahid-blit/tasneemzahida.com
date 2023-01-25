e848cb91dfdec142cd9eedf6f5970624d4100d9f
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2023-01-25 18:25:08 +0500

    Server file test

--- src/routes/events/[id]/+page.svelte
+++ src/routes/events/[id]/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-  import type { ParsedEvent } from "src/types/ParsedEvent";
+  // import type { ParsedEvent } from "src/types/ParsedEvent";
   import type { PageData } from "./$types";
 
   export let data: PageData;
-  let event: ParsedEvent = data.event;
-  let { blog, text } = event;
+  // let event: ParsedEvent = data.event;
+  // let { blog, text } = event;  
 </script>
 
-<h1 class="text-primary">{blog.title}</h1>
-<img src={new URL(`/src/lib/assets/events/${blog.imgFilename}`, import.meta.url).href} alt="About the event">
-<div class="text-primary text-2xl">{@html text}</div>
+<p>Here is the link: {data.goofyAhhh}</p>
+
+<!-- <h1 class="text-primary">{blog.title}</h1> -->
+<!-- <img src={new URL(`/src/lib/assets/events/${blog.imgFilename}`, import.meta.url).href} alt="About the event"> -->
+<!-- <div class="text-primary text-2xl">{@html text}</div> -->
 
--- src/routes/events/[id]/+page.ts
+++ src/routes/events/[id]/+page.ts
@@ -1,12 +1,24 @@
-import { error } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import { marked } from 'marked';
+import Blogs from '$lib/assets/events.json';
+import type { EventBlog } from 'src/types/EventBlog';
 import type { ParsedEvent } from 'src/types/ParsedEvent';
+
+export const ssr = false;
+export const csr = true;
 
 import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ fetch, params }) => {
-	const response = await fetch(`/api/events/${params.id}`);
-	const event: ParsedEvent = await response.json();
+	// const response = await fetch(`/api/events/${params.id}`);
+	// const event: ParsedEvent = await response.json();
+	// return {
+	// 	event: event
+	// };
+	const eventUrl = new URL(`/src/lib/assets/events/heheevent.md`, import.meta.url).href;
+	const response = await fetch(eventUrl);
+	const result = await response.text();
+	console.log('Markdown: ', result);
 	return {
-		event: event
+		goofyAhhh: result
 	};
 };
