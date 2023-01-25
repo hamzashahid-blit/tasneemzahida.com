b4a5cb83227e266b8f5221dc409436b9a2679d6e
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2023-01-25 14:58:31 +0500

    Re-add event files

---
+++ src/routes/events/[id]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { ParsedEvent } from "src/types/ParsedEvent";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+  let event: ParsedEvent = data.event;
+  let { blog, text } = event;
+</script>
+
+<h1 class="text-primary">{blog.title}</h1>
+<img src={new URL(`/src/lib/assets/events/${blog.imgFilename}`, import.meta.url).href} alt="About the event">
+<div class="text-primary text-2xl">{@html text}</div>
+
