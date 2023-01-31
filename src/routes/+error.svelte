09fd2dbeefc4c17ea928680a3ee3b30f601dd0cc
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2023-01-31 18:44:31 +0500

    Add +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<div class="min-h-[calc(100vh-theme(space.28)-theme(space.10))] w-full flex items-center justify-center mb-10 -my-10">
+  <h2 class="text-5xl text-primary">
+	<span class="text-gray-500">[{$page.status}]</span>
+	{$page.error.message}
+  </h2>
+</div>
