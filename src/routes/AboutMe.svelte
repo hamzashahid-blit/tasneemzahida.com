5b12223d36e7e83402780e7b655bfa6ee1a772ee
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2023-02-03 21:13:07 +0500

    Complete "Contact Me" page

--- src/routes/AboutMe.svelte
+++ src/routes/AboutMe.svelte
@@ -1,7 +1,7 @@
 <div id="about-me" class="flex flex-col lg:flex-row items-center lg:justify-evenly lg:space-x-20 text-primary min-h-[calc(100vh-theme(space.28))] lg:-mt-10 mb-36 mx-6 lg:mx-20">
   <div class="order-2 lg:order-1 flex flex-col">
-    <h1 class="text-5xl lg:text-6xl mb-2 -mt-4">About <span class="font-fancy text-5xl lg:text-7xl -ml-2">me</span></h1>
-	<p class="text-2xl max-w-2xl">Nam vestibulum accumsan nisl. Pellentesque tristique imperdiet tortor.  Praesent augue.  Nunc eleifend leo vitae magna.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.</p>
+    <h1 class="text-5xl lg:text-6xl mb-4 -mt-4">About <span class="font-fancy text-5xl lg:text-7xl -ml-2">me</span></h1>
+	<p class="text-2xl max-w-2xl">Tasneem Zahida is a Pakistani artist who was brought up in Jeddah, Saudi Arabia. She takes inspiration from the Holy Quran and attempts to make it more accessible to the modern generation. Her journey of art started when she took fine arts as one of her majors in high school and soon developed a keen interest in painting. She then joined the Saudi Arts Center and further enhanced her skills from the Art Academy of Canada while also learning Arabic calligraphy from various calligraphists.</p>
   </div>
   <img class="order-1 lg:order-2 max-h-57 rounded-xl drop-shadow-lg max-w-full flex lg:my-0 my-16" src="/about-me.jpg" alt="Portrait">
 <!-- 5px_5px_0px_20px_rgb(100,20,200,1) -->
--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,3 +1,60 @@
-<div class="min-h-[calc(100vh-theme(space.28)-theme(space.10))] w-full flex items-center justify-center mb-10 -my-10">
-  <h1 class="text-5xl text-primary">Coming Soon...</h1>
+<script lang="ts">
+  import Icon from 'svelte-awesome';
+  import email from 'svelte-awesome/icons/envelopeO';
+  import instagram from 'svelte-awesome/icons/instagram';
+  import phone from 'svelte-awesome/icons/phone';
+  import facebook from 'svelte-awesome/icons/facebookSquare';
+</script>
+
+<div class="min-h-[calc(100vh-theme(space.28)-theme(space.10)-theme(space.4))] w-full flex items-center justify-center mb-10 -my-10">
+  <div>
+	 <h1 class="text-hxl text-primary mb-10">Contacts</h1>
+	 <div class="flex items-center justify-center space-x-14">
+	   <table class="table-auto text-primary text-3xl mb-3">
+	     <tbody>
+	      <!-- Mail -->
+	      <tr>
+	       <td class="text-center align-middle pr-4 pb-3">
+			 <a href="mailto:tasneemzart@gmail.com"><Icon data={email} scale={2}/></a></td>
+	       <td class="pb-3"><a href="mailto:tasneemzart@gmail.com">tasneemzart@gmail.com</a></td>
+	      </tr>
+	      <!-- Phone -->
+	      <tr>
+	       <td class="text-center align-middle pr-4"><Icon data={phone} scale={2}/></td>
+	       <td>6478192962</td>
+	      </tr>
+	     </tbody>
+	   </table>
+	   <table class="table-auto text-primary text-3xl mb-3">
+	     <tbody>
+	      <!-- Instagram -->
+		  <tr>
+		    <td class="text-center align-middle pb-1 pr-4 pb-3">
+	          <a class="text-primary" href="https://www.instagram.com/tasneemzahida_art/">
+			    <Icon data={instagram} scale={2}/>
+	          </a>
+	        </td>
+		    <td class="pb-3">
+	          <a class="text-primary" href="https://www.instagram.com/tasneemzahida_art/">
+			    @tasneemzahida_art
+	          </a>
+		    </td>
+		  </tr>
+	      <!-- Facebook -->
+		  <tr>
+		    <td class="text-center align-middle pb-1 pr-4">
+	          <a class="text-primary" href="https://www.facebook.com/tasneem.zahida.1">
+			    <Icon data={facebook} scale={1.85}/>
+			  </a>
+		    </td>
+		    <td class="">
+	          <a class="text-primary" href="https://www.facebook.com/tasneem.zahida.1">
+			    Tasneem Zahida
+			  </a>
+		    </td>
+		  </tr>
+	     </tbody>
+	   </table>
+	 </div>
+  </div>
 </div>
