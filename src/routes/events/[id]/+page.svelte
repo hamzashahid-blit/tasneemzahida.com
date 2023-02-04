d0d42b486b046fea006c0335ae12be4980ad3924
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2023-02-04 14:43:18 +0500

    Add events/[ID] Pages

--- src/routes/events/[id]/+page.svelte
+++ src/routes/events/[id]/+page.svelte
@@ -12,27 +12,27 @@
 <!--   <p>Here is the link: {data.events}</p> -->
 <!-- {/await} -->
 
-<div class="flex flex-col justify-center mx-60 my-28">
-  <h1 class="text-hxl text-primary">{event.title}</h1>
-  <img class="rounded-md mt-5 mb-10" src={new URL(`/src/lib/assets/events/${event.id}.png`, import.meta.url).href} alt="About the event">
+<div class="flex flex-col justify-center mx-4 lg:mx-60 my-12 lg:my-28">
+  <h1 class="text-5xl lg:text-hxl text-primary">{event.title}</h1>
+  <img class="rounded-sm lg:rounded-md mt-5 mb-10" src={new URL(`/src/lib/assets/events/${event.id}.png`, import.meta.url).href} alt="About the event">
   <div id="md" class="text-primary">{@html event.content}</div>
 </div>
 
 <style>
   #md :global(h1) {
-	@apply text-5xl
+	@apply text-4xl lg:text-5xl
   }
   #md :global(h2) {
-	@apply text-4xl
+	@apply text-3xl lg:text-4xl
   }
   #md :global(h3) {
-	@apply text-3xl
+	@apply text-2xl lg:text-3xl
   }
   #md :global(h4) {
-	@apply text-2xl
+	@apply text-xl lg:text-2xl
   }
   #md :global(h5) {
-	@apply text-xl
+	@apply text-[1.6em] lg:text-xl
   }
   #md :global(p) {
 	@apply text-2xl
