a5e3fc4c4168881696a78c8304cd4e053f05b1d4
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2023-02-04 14:44:01 +0500

    Remove unnecessary debug bg-color

--- src/lib/components/Category.svelte
+++ src/lib/components/Category.svelte
@@ -12,7 +12,7 @@
 	  <a data-sveltekit-prefetch href="/paintings/{id}">
 	    <div class="brick before:content-[''] before:absolute before:w-full before:h-1/2 before:top-full before:left-0 before:bg-gradient-to-b before:from-transparent before:to-[rgb(0,0,0,0.4)] before:transition-all before:duration-300
 				    relative mb-8 transition-all overflow-hidden rounded-sm">
-		  <img class="bg-red-200 w-full" src={new URL(`/src/lib/assets/${category}/${filename}`, import.meta.url).href} alt={title}>
+		  <img class="w-full" src={new URL(`/src/lib/assets/${category}/${filename}`, import.meta.url).href} alt={title}>
 		  <h2 class="absolute bottom-3 left-5 text-white text-2xl lg:text-3xl text-heading w-full text-left transition-all">{title}</h2>
 		  <!-- <h2 class="absolute top-[68%] left-[5%] right-[5%] text-white text-4xl">{title}</h2> -->
 		  <!-- <p class="absolute top-[82%] left-[5%] right-[5%] text-white text-xl leading-5 line-clamp-2">{description}</p> -->
