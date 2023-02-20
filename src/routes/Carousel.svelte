52fd3d201479aeb2da040e8af19ac9db0ee66d88
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2023-02-20 17:08:16 +0500

    Oops, untracked files

---
+++ src/routes/Carousel.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { Painting } from "src/types/Painting";
+  export let paintings: Painting[]
+</script>
+
+<ul class="flex overflow-x-auto gap-6 items-center snap-x snap-mandatory before:w-6 after:w-6">
+  {#each paintings as painting}
+	<li class="shrink-0 snap-center snap-always w-[80vw]">
+	  <a data-sveltekit-prefetch href="/paintings/{painting.id}">
+        <img class="rounded-md" src={`/src/lib/assets/${painting.category}/${painting.filename}`} alt="{painting.title}">
+	  </a>
+	</li>
+  {/each}
+</ul>
