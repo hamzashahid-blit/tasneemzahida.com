366de2b3e58c33804f7a55f0af35ed071ea09dd9
Your Name <you@example.com>
2024-12-28 14:55:15 +0500

    Fix overflowing components

--- src/routes/Categories.svelte
+++ src/routes/Categories.svelte
@@ -1,4 +1,4 @@
-<div id="categories" class="flex flex-col items-center justify-center">
+<div id="categories" class="flex flex-col items-center justify-center mb-2 mx-6">
   <div id="main" alt="A painting showing main gallery">
 	<h1>Main Gallery</h1>
 	<a href="/main" data-sveltekit-prefetch>View more</a>
@@ -12,10 +12,11 @@
 	<a href="/other" data-sveltekit-prefetch>View more</a>
   </div>
 </div>
+
 <style>
   #categories > div {
     /* @apply grid grid-rows-3 md:grid-rows-none md:grid-cols-3 items-center justify-items-center h-[50vh] md:h-[31vh] bg-cover bg-center transition-all duration-300 mx-4 my-2 rounded-xl drop-shadow-lg text-white text-center px-2 py-1 */
-	@apply flex flex-col items-center justify-center text-white text-center h-[50vh] md:h-[31vh] w-[98vw] bg-cover bg-center transition-all duration-300 mx-10 my-2 rounded-xl drop-shadow-lg px-2 py-1
+	@apply flex flex-col items-center justify-center text-white text-center h-[50vh] md:h-[31vh] w-full bg-cover bg-center transition-all duration-300 my-2 rounded-xl drop-shadow-lg px-2 py-1
   }
   #main {
     background-image:
