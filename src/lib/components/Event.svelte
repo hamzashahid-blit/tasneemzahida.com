6ef2f8846be75493ba19ab18b3ffc311beabf117
Your Name <you@example.com>
2025-01-06 14:52:58 +0500

    Allow wonky imgs for events

--- src/lib/components/Event.svelte
+++ src/lib/components/Event.svelte
@@ -6,9 +6,11 @@
 </script>
 
 <a data-sveltekit-prefetch href="/events/{event.id}" class="flex flex-col items-center max-w-[20rem] md:max-w-full max-h-96 text-primary bg-softgray rounded-md transition-all hover:scale-105"> <!-- box-shadow-[10px_10px_10px_rgba(255,0,0,0.25)] -->
-  <img class="rounded-xl" src={new URL(`/src/lib/assets/events/${event.picture}`, import.meta.url).href} alt={event.title}>
+  <div class="rounded-t-xl flex overflow-hidden">
+    <img class="rounded-t-xl self-center justify-self-center" src={new URL(`/src/lib/assets/events/${event.picture}`, import.meta.url).href} alt={event.title}>
+  </div>
   <h1 class="my-[0.6rem] lg:mx-[1rem] text-center font-heading text-2xl leading-7">{event.title}</h1>
-  <p class="text-xl text-softprimary text-ellipsis align-bottom self-end overflow-hidden leading-tight mx-5">{event.description}</p>
+  <p class="max-h-[50%] text-xl text-softprimary text-ellipsis align-bottom self-end overflow-hidden leading-tight mx-5">{event.description}</p>
   <div class="flex items-end justify-between w-[calc(100%-theme(space.10))] m-2">
 	<EventFooter date={new Date(event.date)} textLength={event.content.length}/>
   </div>
