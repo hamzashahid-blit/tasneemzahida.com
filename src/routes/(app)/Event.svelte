507069105dde97ee29d49c649f09f19f9b6a5295
Your Name <you@example.com>
2025-05-08 12:01:57 +0500

    Fix Recent Events Not Showing Cover Image

--- src/routes/(app)/Event.svelte
+++ src/routes/(app)/Event.svelte
@@ -8,7 +8,7 @@
 <!-- <h1>Coming Soon...</h1> -->
 
 <a data-sveltekit-prefetch href="/events/{event.id}" class="flex flex-col items-center max-w-[18rem] lg:max-w-[20rem] text-primary bg-softgray rounded-xl transition-all hover:scale-105 lg:hover:scale-110"> <!-- box-shadow-[10px_10px_10px_rgba(255,0,0,0.25)] -->
-  <img class="rounded-t-xl w-[20rem]" src={new URL(`/src/lib/assets/events/${event.id}.png`, import.meta.url).href} alt="The event">
+  <img class="rounded-t-xl w-[20rem]" src={new URL(`/src/lib/assets/events/${event.picture}`, import.meta.url).href} alt="The event">
   <div class="p-5">
     <h1 class="mb-2 text-center font-heading text-2xl leading-7">{event.title}</h1>
     <p class="text-xl text-softprimary text-ellipsis leading-tight h-[15vh] lg:h-[25vh] overflow-hidden">{event.description}</p>
